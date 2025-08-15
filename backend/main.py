18fa062e30752807b9df35614d3cc6ee5785d0ca
Daniel-Ugulino <danielugulino46@gmail.com>
2025-08-15 19:02:47 -0300

    fix: removed the on startup event

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from utils.dataloader import companyLoader
 from utils.scraper import Scraper
 import asyncio
 import logging
@@ -9,13 +8,6 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def startup_event():
-    await companyLoader()
-    await asyncio.sleep(5)
-    scraper = Scraper()
-    await scraper.run()
     
 
-def cronJob():
-    return {"Hello": "World"}
+
